93aeb7d1bf1d58814ac8505bbc49e23574544192
JesusVale <108861698+JesusVale@users.noreply.github.com>
2023-03-30 16:24:57 -0700

    Se agregó parte de la pantalla home

--- app/src/main/java/mx/itson/edu/keynote/HomeFragment.kt
+++ app/src/main/java/mx/itson/edu/keynote/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,14 +28,18 @@ class HomeFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val myFragmentView: View? = inflater.inflate(R.layout.fragment_home, container, false)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return myFragmentView
     }
 
     companion object {
