413ab6ecfc86f653b2e2e28f4eedc7100269e2b4
JesusVale <108861698+JesusVale@users.noreply.github.com>
2023-03-30 20:15:26 -0700

    Se agregó la pantalla Home

--- app/src/main/java/mx/itson/edu/keynote/HomeFragment.kt
+++ app/src/main/java/mx/itson/edu/keynote/HomeFragment.kt
@@ -1,11 +1,18 @@
 package mx.itson.edu.keynote
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
 import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +28,7 @@ class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    var tareas: ArrayList<Nota> = ArrayList<Nota>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,9 +46,72 @@ class HomeFragment : Fragment() {
 
         val myFragmentView: View? = inflater.inflate(R.layout.fragment_home, container, false)
 
+        val layoutManager = GridLayoutManager(this.activity, 1, GridLayoutManager.HORIZONTAL, false)
+        val layoutManagerUltimas = LinearLayoutManager(this.activity, LinearLayoutManager.HORIZONTAL, false)
+        val layoutManagerFijadas = LinearLayoutManager(this.activity, LinearLayoutManager.HORIZONTAL, false)
+
+
+        val recyclerHorario:RecyclerView = myFragmentView!!.findViewById(R.id.recyclerHorario)
+        val recyclerUltimas:RecyclerView = myFragmentView!!.findViewById(R.id.recyclerUltimas)
+        val recyclerFijadas:RecyclerView = myFragmentView!!.findViewById(R.id.recyclerFijadas)
+
+        recyclerHorario.setLayoutManager(layoutManager)
+        recyclerUltimas.setLayoutManager(layoutManagerUltimas)
+        recyclerFijadas.setLayoutManager(layoutManagerFijadas)
+
+        agregarNotas()
+
+        val adapterNotas:AdapterNotas = AdapterNotas(tareas)
+        recyclerHorario.adapter = adapterNotas
+        recyclerUltimas.adapter = adapterNotas
+        recyclerFijadas.adapter = adapterNotas
+
         // Inflate the layout for this fragment
         return myFragmentView
     }
+
+    fun agregarNotas(){
+        tareas.add(Nota("1:00 - 2:00 Ejercicio", "Tu me dices que no tienes ritmo, pues mira lo que acabas de hacer, yo ya tengo un trabajo muy bueno", R.drawable.navyblue_note_background))
+        tareas.add(Nota("10:00 - 12:00 Tarea", "Matematicas Realizar los siguientes ejercicios...", R.drawable.blue_note_background))
+        tareas.add(Nota("11:00 - 12:00 Dibujo", "EOOOOO E000 EEO EEO EEEEO EEEEE0 EO EO EO EO ALL RIGHT ALL RIGHT!!", R.drawable.orange_note_background))
+        tareas.add(Nota("3:00 - 4:00 Canto", "Que sepa el mundo que en marcha estoy, que voy a cumplir mi misión", R.drawable.red_note_background))
+    }
+
+    class AdapterNotas(var tareas: ArrayList<Nota> ): RecyclerView.Adapter<HomeFragment.AdapterNotas.ViewHolder>(){
+
+        class ViewHolder(view:View): RecyclerView.ViewHolder(view){
+            val titulo:TextView = view.findViewById(R.id.titulo)
+            val contenido:TextView = view.findViewById(R.id.contenido)
+            var layoutNota:LinearLayout = view.findViewById(R.id.layoutNota)
+            init {
+                titulo
+                contenido
+                layoutNota
+            }
+
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            var inflador = LayoutInflater.from(parent.context)
+            var vista = inflador.inflate(R.layout.nota_view, null)
+
+            return ViewHolder(vista)
+
+        }
+
+        override fun getItemCount(): Int {
+            return tareas.size
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, pos: Int) {
+            val tarea = tareas[pos]
+            holder.titulo.setText(tarea.titulo)
+            holder.contenido.setText(tarea.contenido)
+            holder.layoutNota.setBackgroundResource(tarea.background)
+        }
+
+    }
+
 
     companion object {
         /**
---
+++ app/src/main/java/mx/itson/edu/keynote/Nota.kt
@@ -0,0 +1,3 @@
+package mx.itson.edu.keynote
+
+data class Nota(var titulo: String, var contenido: String, var background: Int)
