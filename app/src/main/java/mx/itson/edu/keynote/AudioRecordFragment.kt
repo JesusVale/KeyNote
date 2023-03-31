6e5074f01382c4d01f6d1bda04903af659b203b2
Hugo Rivera <hriveravz@gmail.com>
2023-03-30 20:45:40 -0700

    commit camara, audio etc

---
+++ app/src/main/java/mx/itson/edu/keynote/AudioRecordFragment.kt
@@ -0,0 +1,11 @@
+package mx.itson.edu.keynote
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AudioRecordFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_audio_record_fragment)
+    }
+}
