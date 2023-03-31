7af4ae043804da67c70c7da53301201ddb798c70
MarcoIrineo <marco.irineo228880@potros.itson.edu.mx>
2023-03-30 21:00:54 -0700

    3/4 pantallas
    ea

---
+++ app/src/main/java/mx/itson/edu/keynote/registrar_usuario.kt
@@ -0,0 +1,11 @@
+package mx.itson.edu.keynote
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class registrar_usuario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registrar_usuario)
+    }
+}
