ecd3043dfe6c5855c40919c685f2e45725e3616f
MarcoIrineo <marco.irineo228880@potros.itson.edu.mx>
2023-05-15 23:53:17 -0700

    imagenes problema de guardado

--- app/src/main/java/mx/itson/edu/keynote/Notas.kt
+++ app/src/main/java/mx/itson/edu/keynote/Notas.kt
@@ -1,8 +1,10 @@
 package mx.itson.edu.keynote
 
 import android.app.Activity
+import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
@@ -23,6 +25,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import java.io.ByteArrayOutputStream
+import java.io.File
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -38,10 +44,11 @@ class Notas : Fragment() {
 
     private lateinit var imgView: ImageView
     // TODO: Rename and change types of parameters
+    private val file = 1
     private var param1: String? = null
     private var param2: String? = null
     private val noteRef= FirebaseDatabase.getInstance().getReference("Notes")
-    private var imagen = null
+    private lateinit var imagen: String
     private var editMode:Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,7 +65,7 @@ class Notas : Fragment() {
     ): View? {
 
         val myFragmentView: View? = inflater.inflate(R.layout.fragment_notas, container, false)
-        val btn_camara: ImageButton = myFragmentView!!.findViewById(R.id.btn_photo)
+        val btn_camara: ImageView = myFragmentView!!.findViewById(R.id.btn_photo)
         val btn_audio: ImageButton = myFragmentView!!.findViewById(R.id.btn_voice)
         val btn_save: ImageButton = myFragmentView!!.findViewById(R.id.btn_save)
         val btn_delete: Button = myFragmentView!!.findViewById(R.id.btn_delete)
@@ -87,7 +94,7 @@ class Notas : Fragment() {
         }
 
         btn_save.setOnClickListener{
-            var nota: Note = Note(UserSingleton.getUsuario().id,tituloNota.text.toString(), contenidoNota.text.toString(), "Normal", "")
+            var nota: Note = Note(UserSingleton.getUsuario().id,tituloNota.text.toString(), contenidoNota.text.toString(), "Normal", imagen)
             if(editMode){
                 val id = requireArguments().getString("id")
                 nota.id = id
@@ -114,17 +121,41 @@ class Notas : Fragment() {
     }
 
     private fun abrirCamara(){
-        val intent: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+      /**  val intent: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         if (intent.resolveActivity(requireActivity().packageManager) != null) {
             startActivityForResult(intent, 1)
         }
+**/
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*"
+        startActivityForResult(intent, file)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-            imgView.setImageBitmap(imageBitmap)
-        }
+       if (requestCode == file) {
+            if (resultCode == RESULT_OK) {
+                val FileUri = data!!.data
+                val Folder: StorageReference =
+                    FirebaseStorage.getInstance().getReference().child("images")
+
+                val file_name: StorageReference = Folder.child("file" + FileUri!!.lastPathSegment)
+                file_name.putFile(FileUri).addOnSuccessListener { taskSnapshot ->
+                    file_name.getDownloadUrl().addOnSuccessListener { uri ->
+                     /**   val hashMap =
+                            HashMap<String, String>()
+                        hashMap["link"] = java.lang.String.valueOf(uri)
+                        noteRef.setValue(hashMap) **/
+                        imagen = java.lang.String.valueOf(uri)
+                        Log.d("Mensaje", "Se subió correctamente")
+                    }
+                }
+            }
+       }
+            // Convertir el Bitmap a ByteArray
+           // val baos = ByteArrayOutputStream()
+            //imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+            //imagen = baos.toByteArray()
+
     }
 
     private fun guardarFirebase(note: Note){
@@ -132,6 +163,30 @@ class Notas : Fragment() {
         noteRef.child(userId).setValue(note)*/
 //        val db = Firebase.firestore
 //        val collection = db.collection("Notes")
+        /**Log.d("error","error")
+        if (imagen != null) {
+            val storageRef = FirebaseStorage.getInstance().reference
+            val imageRef = storageRef.child("images/${note.id}.jpg")
+
+            val uploadTask = imageRef.putBytes(imagen!!)
+            uploadTask.addOnSuccessListener {
+                // Imagen subida exitosamente, puedes obtener la URL de descarga
+                imageRef.downloadUrl.addOnSuccessListener { uri ->
+                    val imageUrl = uri.toString()
+                    // Guardar la URL de la imagen en la base de datos
+                    // ...
+
+
+                }.addOnFailureListener { exception ->
+                    // Ocurrió un error al obtener la URL de la imagen
+                    Log.e("UploadImage", "Error getting image URL", exception)
+                }
+            }.addOnFailureListener { exception ->
+                // Ocurrió un error al subir la imagen
+                Log.e("UploadImage", "Error uploading image", exception)
+            }
+        }
+**/
         val noteId= noteRef.push().key!!
 
         noteRef.child(noteId).setValue(note).addOnSuccessListener { documentReference ->
@@ -167,7 +222,7 @@ class Notas : Fragment() {
             "titulo" to note.titulo,
             "contenido" to note.contenido
         )
-        noteRef.child(note.id!!).setValue(nuevosValores)
+        noteRef.child(note.id!!).setValue(note)
         Toast.makeText(this.context, "Se guardaron los cambios correctamente", Toast.LENGTH_SHORT)
             .show()
     }
